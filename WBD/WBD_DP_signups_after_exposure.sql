7799503000e927e97b5e97626ef718423e388205
katez-ds <kate.z@doordash.com>
2026-02-04 16:59:22 -0800

    Update WBD_DP_signups_after_exposure.sql

--- WBD/WBD_DP_signups_after_exposure.sql
+++ WBD/WBD_DP_signups_after_exposure.sql
@@ -26,13 +26,18 @@ case when DATEADD(day,-30,first_eligible_dt) <= START_TIME AND START_TIME < firs
   when DATEADD(day,90,first_eligible_dt) <= START_TIME AND START_TIME < DATEADD(day,120,first_eligible_dt) then 4
   when DATEADD(day,120,first_eligible_dt) <= START_TIME AND START_TIME < DATEADD(day,150,first_eligible_dt) then 5
   when DATEADD(day,150,first_eligible_dt) <= START_TIME AND START_TIME < DATEADD(day,180,first_eligible_dt) then 6
+  when DATEADD(day,180,first_eligible_dt) <= START_TIME AND START_TIME < DATEADD(day,210,first_eligible_dt) then 7
+  when DATEADD(day,210,first_eligible_dt) <= START_TIME AND START_TIME < DATEADD(day,240,first_eligible_dt) then 8
+  when DATEADD(day,240,first_eligible_dt) <= START_TIME AND START_TIME < DATEADD(day,270,first_eligible_dt) then 9
+  when DATEADD(day,270,first_eligible_dt) <= START_TIME AND START_TIME < DATEADD(day,300,first_eligible_dt) then 10
+  when DATEADD(day,300,first_eligible_dt) <= START_TIME AND START_TIME < DATEADD(day,330,first_eligible_dt) then 11
+  when DATEADD(day,330,first_eligible_dt) <= START_TIME AND START_TIME < DATEADD(day,360,first_eligible_dt) then 12
   end as eligible_month_n,
 count(distinct case when dashpass_signup = 1 then subscription_id end) as users
 from proddb.katez.wbd_first_eligible_cohort a
 left join proddb.katez.dp_signups_0126 b
  on a.consumer_id=b.consumer_id
- and start_time::date between DATEADD(day,-30,first_eligible_dt) and DATEADD(day,180,first_eligible_dt)
-where first_eligible_dt <= DATEADD(day,-180,date'2026-02-02')
+ and start_time::date between DATEADD(day,-30,first_eligible_dt) and DATEADD(day,360,first_eligible_dt)
+where first_eligible_dt <= DATEADD(day,-360,date'2026-02-02')
 group by 1,2
 order by 1,2
-
