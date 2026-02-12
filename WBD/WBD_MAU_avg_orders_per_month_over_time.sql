8170c8455e8da110360ccf1fc7e97f35f28263d8
katez-ds <kate.z@doordash.com>
2026-02-12 14:58:53 -0800

    Update WBD_MAU_avg_orders_per_month_over_time.sql

--- WBD/WBD_MAU_avg_orders_per_month_over_time.sql
+++ WBD/WBD_MAU_avg_orders_per_month_over_time.sql
@@ -26,6 +26,24 @@ TAG_RENAMED	ELIGIBLE_CUSTOMERS
 Control	6290772
 Treatment	119563951
 
+-- Total MAU
+select count(distinct creator_id) MAU,
+ count(distinct case when is_subscribed_consumer = FALSE then creator_id end) classic_MAU
+FROM proddb.public.dimension_deliveries dd
+LEFT JOIN edw.cng.dimension_new_vertical_store_tags nv 
+        ON dd.store_id = nv.store_id AND nv.is_filtered_mp_vertical = 1
+WHERE dd.is_filtered_core = TRUE
+        -- AND dd.is_consumer_pickup = FALSE -- optional to exclude pickup orders
+        AND dd.fulfillment_type NOT IN ('dine_in', 'shipping', 'merchant_fleet', 'virtual') -- excluding dine-in, shipping, merchant fleet, and virtual orders (giftcards)
+        AND dd.is_from_store_to_us = FALSE -- excluding store-to-us orders
+        --AND dd.is_bundle_order = FALSE -- excluding bundle orders -- an optional column to filter out DoubleDash
+        --AND nv.business_line IS NULL -- restaurant orders 
+        AND dd.country_id = 1 -- US only 
+        AND dd.created_at::date between date'2026-01-01' and date'2026-01-28'
+
+MAU	CLASSIC_MAU
+36149549	18837370
+
 -- Control vs Treatment: orders by months from first eligible
 select tag_renamed,
 case when DATEADD(day,-30,first_eligible_dt) <= order_dt AND order_dt < first_eligible_dt then -1
