e64a9b6be9bf577d6187f747d4ac3278d46cfce7
katez-ds <kate.z@doordash.com>
2026-02-11 22:50:43 -0800

    Update WBD_MAU_avg_orders_per_month_over_time.sql

--- WBD/WBD_MAU_avg_orders_per_month_over_time.sql
+++ WBD/WBD_MAU_avg_orders_per_month_over_time.sql
@@ -35,13 +35,19 @@ case when DATEADD(day,-30,first_eligible_dt) <= order_dt AND order_dt < first_el
   when DATEADD(day,90,first_eligible_dt) <= order_dt AND order_dt < DATEADD(day,120,first_eligible_dt) then 4
   when DATEADD(day,120,first_eligible_dt) <= order_dt AND order_dt < DATEADD(day,150,first_eligible_dt) then 5
   when DATEADD(day,150,first_eligible_dt) <= order_dt AND order_dt < DATEADD(day,180,first_eligible_dt) then 6
+  when DATEADD(day,180,first_eligible_dt) <= order_dt AND order_dt < DATEADD(day,210,first_eligible_dt) then 7
+  when DATEADD(day,210,first_eligible_dt) <= order_dt AND order_dt < DATEADD(day,240,first_eligible_dt) then 8
+  when DATEADD(day,240,first_eligible_dt) <= order_dt AND order_dt < DATEADD(day,270,first_eligible_dt) then 9
+  when DATEADD(day,270,first_eligible_dt) <= order_dt AND order_dt < DATEADD(day,300,first_eligible_dt) then 10
+  when DATEADD(day,300,first_eligible_dt) <= order_dt AND order_dt < DATEADD(day,330,first_eligible_dt) then 11
+  when DATEADD(day,330,first_eligible_dt) <= order_dt AND order_dt < DATEADD(day,360,first_eligible_dt) then 12
   end as eligible_month_n,
 count(distinct b.delivery_id) as orders
 from proddb.katez.wbd_first_eligible_cohort a
 left join proddb.katez.consumer_orders b
 on a.consumer_id = b.consumer_id
-and order_dt between DATEADD(day,-30,first_eligible_dt) and DATEADD(day,180,first_eligible_dt)
-where first_eligible_dt <= DATEADD(day,-180,date'2026-02-02')
+and order_dt between DATEADD(day,-30,first_eligible_dt) and DATEADD(day,360,first_eligible_dt)
+where first_eligible_dt <= DATEADD(day,-360,date'2026-02-02')
 group by 1,2
 order by 1,2
 
