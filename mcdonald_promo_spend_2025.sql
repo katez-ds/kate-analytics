31134eeba3bf2d6311f6179ee94c5ca57cae044c
katez-ds <kate.z@doordash.com>
2026-03-09 15:59:40 -0700

    Update mcdonald_promo_spend_2025.sql

--- mcdonald_promo_spend_2025.sql
+++ mcdonald_promo_spend_2025.sql
@@ -11,11 +11,22 @@ sum(crm_discount) crm_discount,
     sum(mx_funded_cx_discount) mx_funded_discount,
     sum(crm_discount) crm_discount
     from proddb.public.dimension_deliveries b 
-      left join proddb.static.df_sf_promo_discount_delivery_level a
+    left join proddb.static.df_sf_promo_discount_delivery_level a
       on a.delivery_id = b.delivery_id
+    left join 
+        (select delivery_id,
+        case when campaign_id is not null then coalesce(FDA_OTHER_PROMOTIONS_BASE + FDA_PROMOTION_CATCH_ALL + FDA_CONSUMER_RETENTION - FDA_BUNDLES_PRICING_DISCOUNT, 0) else 0 end AS crm_discount
+        from proddb.public.fact_order_discounts_and_promotions_extended
+        where crm_discount >0
+        and year(active_date) = 2025
+        group by 1,2) c
+        on b.delivery_id = c.delivery_id
     where
       1 = 1
-      and order_dt between '2025-01-01' and '2025-12-31'
+      and b.created_at:: date between '2025-01-01' and '2025-12-31'
       and b.created_at:: date between '2025-01-01' and '2025-12-31'
       and country_id = 1
       and business_id = 5579
+
+AFFORDABILITY_PROGRAM_DISCOUNT	MX_FUNDED_DISCOUNT	CRM_DISCOUNT
+66014232.640000	286931195.430000	35207169.713421703436
