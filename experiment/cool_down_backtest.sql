21d404fb09512d3c21d6ad152d189bb30eaff6fc
katez-ds <kate.z@doordash.com>
2026-04-16 10:28:52 -0700

    Update cool_down_backtest.sql

--- experiment/cool_down_backtest.sql
+++ experiment/cool_down_backtest.sql
@@ -10,6 +10,8 @@ with
       static.us_universal_dv_a_be a
       join proddb.static.wbd_experiment_exposure b --old DV in treatment
             on a.user_id = b.user_id and b.tag_renamed = 'Treatment'
+      join proddb.public.FACT_DYNAMIC_AUDIENCE_WBD_ORDER_FREQUENCY_L365D c
+        on injected_date = first_exposed::date and c.consumer_id = a.user_id
   group by all
   ),
   core_dd as (
